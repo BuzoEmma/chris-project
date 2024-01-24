c073bd667caf1e6108432b2b3897f1eb62f0f35b
BuzoEmma <chibuzoemmanuel55@gmail.com>
2024-01-24 12:41:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Button";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Home>
+        <h2>Class is going on</h2>
+        <h2>Class is going on</h2>
+        <h2>Class is going on</h2>
+      </Home>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "../Button";
+
+const Home = ({ children }) => {
+  const id = "Class";
+
+  return (
+    <div>
+      hello am with the children Lorem ipsum dolor sit amet, consectetur
+      um eligendi saepe maxime debitis culpa minima
+      quis? Fugiat.
+      <h3>{children}</h3>
+    </div>
+  );
+};
+
+export default Home;
